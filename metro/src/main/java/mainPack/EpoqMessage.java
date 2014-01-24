a2291c53f8b0a2de526b1f0b4d2d08f3f003c415
boussad <bsd@>
2014-01-24 08:25:41 +0100

    epoq

---
+++ metro/src/main/java/mainPack/EpoqMessage.java
@@ -0,0 +1,29 @@
+package mainPack;
+
+
+/**
+ * A Message  to be sent to the observers 
+ * */ 
+public class EpoqMessage {
+	
+	private int _value;  // The data of this message 
+	private int _versionNumber;  //  a counter to simulate the epoq strategy 
+	
+
+	public int get_value() {
+		return _value;
+	}
+	public void set_value(int _value) {
+		this._value = _value;
+	}
+	public int get_versionNumber() {
+		return _versionNumber;
+	}
+	public void set_versionNumber(int _versionNumber) {
+		this._versionNumber = _versionNumber;
+	}
+	
+	
+	
+
+}
